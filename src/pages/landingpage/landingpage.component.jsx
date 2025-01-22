3214e18b9505dda57df9b2e2fd16867b79fa6b47
Rishav Kumar <rishavk1102@gmail.com>
2025-01-22 15:17:14 +0530

    Updated images in landing page

--- src/pages/landingpage/landingpage.component.jsx
+++ src/pages/landingpage/landingpage.component.jsx
@@ -1,35 +1,44 @@
-import useMediaQuery from "@mui/material/useMediaQuery";
-import React, { useEffect } from "react";
 import axios from "axios";
+import React, { useEffect } from "react";
 import CarouselPanel from "../../components/carousel/carousel.component";
 import Footer from "../../components/footer/footer.component";
 import Navbar from "../../components/navbar/navbar.component";
 import PromotionImageList from "../../components/promotionImageList/promotions.component";
 import "./landingpage.styles.scss";
 const LandingPage = () => {
-  const matches = useMediaQuery("(min-width:600px)");
   useEffect(() => {
     getLocation();
   }, []);
+
   const getLocation = async () => {
     if ("geolocation" in navigator) {
-      await navigator.geolocation.getCurrentPosition(async function (
-        position
-      ) {
-
+      await navigator.geolocation.getCurrentPosition(async function (position) {
         let locationResponse = await axios.get(
           `https://geocode.maps.co/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&api_key=66d34ff0b8bdb852964430lcwc30d15`
         );
 
         console.log(locationResponse.data.address.postcode);
-        localStorage.setItem("default_pincode", locationResponse.data.address.postcode);
-        localStorage.setItem("default_city", locationResponse.data.address.city);
-        localStorage.setItem("default_state", locationResponse.data.address.state);
-        localStorage.setItem("default_country", locationResponse.data.address.country);
+        localStorage.setItem(
+          "default_pincode",
+          locationResponse.data.address.postcode
+        );
+        localStorage.setItem(
+          "default_city",
+          locationResponse.data.address.city
+        );
+        localStorage.setItem(
+          "default_state",
+          locationResponse.data.address.state
+        );
+        localStorage.setItem(
+          "default_country",
+          locationResponse.data.address.country
+        );
         console.log(locationResponse.data.address);
       });
     }
   };
+
   return (
     <div
       className="landing-page"
